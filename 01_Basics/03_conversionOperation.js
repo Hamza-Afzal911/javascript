3e8b69e62fd8b2c84962582cc36e7d42f2143dbf
Hamza-Afzal911 <163814719+Hamza-Afzal911@users.noreply.github.com>
2024-11-17 20:26:14 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "Hamza"
+
+console.log(typeof score)
+
+let valueInNumber = Numner(score)
+console.log(typeof valueInNumber)
+
+// 33 => "33"
+// "33abs" => NAN
+// true => 1; False => 0
+
+let isLoggIn = "Hamza"
+
+let BooleanIsLogIn = Boolean(isLoggIn)
+console.log(BooleanIsLogIn)
+
+// "" => false
+// "Hamza" => true
